1559c8a8532265055d5da0fc71fdc6e9a6452ba1
Dawei Lin <personal.dawei.lin@gmail.com>
2024-06-21 02:18:13 +0200

    Move some html items

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-console.log('Hello World!')
+function load() {
+    let ninput = document.getElementById('ninput');
+    let range = document.getElementById('slidebar');
+
+    ninput.addEventListener('input', () => {
+        range.value = ninput.value;
+    });
+
+    range.addEventListener('input', () => {
+        ninput.value = range.value;
+    });
+}
